397429c8b36ee159e5260e04308560437605e50c
pabloparejo <parejo.p@gmail.com>
2015-04-17 21:40:32 +0200

    Initial Commit

---
+++ QRcodes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRcodes
+//
+//  Created by Pablo Parejo Camacho on 17/4/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRcodes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QRcodes
+//
+//  Created by Pablo Parejo Camacho on 17/4/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
