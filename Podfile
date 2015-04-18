eeb5526782f0f68396ac207bb7ebbd211b4f40f0
pabloparejo <parejo.p@gmail.com>
2015-04-18 11:21:33 +0200

    added image loading and test button

--- Podfile
+++ Podfile
@@ -1,2 +1,4 @@
 platform :ios, '7.0'
 pod 'QRCodeReaderViewController', '~> 3.3.0'
+pod 'AFNetworking', '~> 2.5.2'
+pod 'MBProgressHud', '~> 0.5'
--- QRcodes/ViewController.m
+++ QRcodes/ViewController.m
@@ -7,21 +7,87 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "QRCodeReaderViewController.h"
+#import "AFNetworking.h"
+#import "MBProgressHUD.h"
+@interface ViewController () <QRCodeReaderDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) QRCodeReaderViewController *reader;
+@property (strong, nonatomic) MBProgressHUD *hud;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)scanAction:(id)sender
+{
+    
+    _reader = [QRCodeReaderViewController new];
+    
+    // Set the presentation style
+    _reader.modalPresentationStyle = UIModalPresentationFormSheet;
+    
+    // Using delegate methods
+    _reader.delegate = self;
+    
+    [self presentViewController:_reader animated:YES completion:NULL];
+}
+
+#pragma mark - QRCodeReader Delegate Methods
+
+- (void)reader:(QRCodeReaderViewController *)reader didScanResult:(NSString *)result
+{
+    __weak typeof(self) weakSelf = self;
+    [self dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"%@", result);
+        [weakSelf loadImageFromUrl:[NSURL URLWithString:result]];
+    }];
+}
+- (IBAction)downloadImage:(id)sender {
+    [self loadImageFromUrl:[NSURL URLWithString:@"http://pili.la/1125"]];
+}
+
+- (void)readerDidCancel:(QRCodeReaderViewController *)reader
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+#pragma mark - Utils
+
+-(void)loadImageFromUrl:(NSURL *)url{
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    if (self.hud == nil) {
+        self.hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+        [self.hud hide:YES];
+        self.hud.mode = MBProgressHUDModeAnnularDeterminate;
+        self.hud.labelText = @"Loading image...";
+    }
+    
+    AFHTTPRequestOperation *requestOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    requestOp.responseSerializer = [AFImageResponseSerializer serializer];
+    __weak typeof(self) weakSelf = self;
+    [requestOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Response: %@", responseObject);
+        weakSelf.imageView.image = responseObject;
+        [weakSelf.hud hide:YES];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Image error: %@", error);
+    }];
+    [requestOp setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+        float progress = (float)totalBytesRead / (float)totalBytesExpectedToRead;
+        weakSelf.hud.progress = progress;
+        NSLog(@"%f", progress);
+    }];
+    
+    [requestOp start];
 }
 
 @end
